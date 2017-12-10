a9b6a371ed11c79f0f3135ed4fe6692af8227d88
Coraliedu35 <34239901+Coraliedu35@users.noreply.github.com>
2017-12-10 16:10:58 +0100

    Update tableauetudiants.php

--- tableauetudiants.php
+++ tableauetudiants.php
@@ -4,7 +4,8 @@
     <?php
     try
     {
-    $bdd = new PDO('mysql:host=localhost;dbname=bdd_promo;charset=utf8', 'root', '');       //lien avec la base de données
+    $bdd = new PDO('mysql:host=localhost;dbname=bdd_promo;charset=utf8', 'root', '',
+    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));                                    //lien avec la base de données
     }
     catch (Exception $e)                                                                    //en cas d'erreur on arrête la page
     {
