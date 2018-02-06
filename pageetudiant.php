ec418e8c664aae0b106fb705e82f6559b4de1939
mgskiller <mgskiller@gmail.com>
2018-02-06 13:14:08 +0100

    Add files via upload

--- pageetudiant.php
+++ pageetudiant.php
@@ -1,4 +1,64 @@
 <?php
-
-// Nous insérons ici la page étudiant (à faire)
+    session_start();
 ?>
+
+*<!DOCTYPE html>
+
+<html>
+    <?php
+    try
+    {
+    $bdd = new PDO('mysql:host=localhost;dbname=bdd_promo;charset=utf8', 'root', '',
+    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));                                    //lien avec la base de données
+    }
+    catch (Exception $e)                                                                    //en cas d'erreur on arrête la page
+    {
+            die('Erreur : ' . $e->getMessage());
+    }
+    ?>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <title>Statistiques d'absence</title>
+    </head>
+    <body>
+        <header> IUT de Saint-Malo </header>
+        <h1>Statistiques individuelles d'absence</h1>
+        <table align="center">
+            <thead>
+                <tr>
+                    <td>Absences justifiées </td>
+                    <td>Absence injustifiées</td>
+                    <td>Absences totales</td>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $reponse = $bdd->prepare('SELECT * FROM bdd_promo.etudiant WHERE login=?');
+                $reponse->execute(array($_SESSION['login']));
+      
+                while ($donnees = $reponse->fetch())    
+                {
+                ?>
+                <tr>
+                    <td><?php echo $donnees['Nb absences J']; ?></td>
+                    <td><?php echo $donnees['Nb absences NJ']; ?></td>
+                    <td><?php echo $donnees['Nb absences NJ']+$donnees['Nb absences J']; ?></td>
+                </tr>
+                <?php
+                }
+
+                $reponse->closeCursor(); // Termine le traitement de la requête
+                ?>
+            </tbody>
+        </table>
+        <!--bouton de déconnexion : voir à créer un pop-up "êtes-vous sûr"-->
+       <form method="POST" action="accueil.php">
+            <input type="submit" value="déconnexion" />
+        </form>
+        <form method="POST" action="faq.html"><input type="submit" value="FAQ" /></form>
+
+    </body>
+
+
+</html>
