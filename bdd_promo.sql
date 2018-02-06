5250cd56249df99fe1c18bb1aa2255f989fb5691
Coraliedu35 <34239901+Coraliedu35@users.noreply.github.com>
2018-02-06 11:13:07 +0100

    Update bdd_promo.sql

--- bdd_promo.sql
+++ bdd_promo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 08 déc. 2017 à 19:36
+-- Généré le :  mar. 06 fév. 2018 à 10:10
 -- Version du serveur :  5.7.19
 -- Version de PHP :  5.6.31
 
@@ -31,10 +31,10 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `cours`;
 CREATE TABLE IF NOT EXISTS `cours` (
   `Prof` text CHARACTER SET utf8 NOT NULL,
-  `N°Salle` text NOT NULL,
+  `salle` int(11) NOT NULL,
   `Heure` time(4) NOT NULL,
   `Date` date NOT NULL,
-  `N°Badgeuse` int(11) NOT NULL
+  `badge` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -47,57 +47,60 @@ DROP TABLE IF EXISTS `etudiant`;
 CREATE TABLE IF NOT EXISTS `etudiant` (
   `Nom` text CHARACTER SET utf8 NOT NULL,
   `Prénom` text CHARACTER SET utf8 NOT NULL,
-  `n°etudiant` int(11) DEFAULT NULL,
+  `photo` text NOT NULL,
+  `login` text CHARACTER SET utf8 COLLATE utf8_bin,
   `MDP` int(11) DEFAULT NULL,
   `Groupe` int(11) NOT NULL,
-  `Nb absences J` int(11) DEFAULT NULL,
-  `Nb absences NJ` int(11) DEFAULT NULL,
-  `n° badge` int(11) DEFAULT NULL
+  `absencesJ` int(11) DEFAULT NULL,
+  `absencesNJ` int(11) DEFAULT NULL,
+  `presence` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `etudiant`
 --
 
-INSERT INTO `etudiant` (`Nom`, `Prénom`, `n°etudiant`, `MDP`, `Groupe`, `Nb absences J`, `Nb absences NJ`, `n° badge`) VALUES
-('Briand-Goudet', 'Loetis', NULL, NULL, 1, NULL, NULL, NULL),
-('Barreau', 'Pierre', NULL, NULL, 1, NULL, NULL, NULL),
-('Blanchon', 'Erwan', NULL, NULL, 1, NULL, NULL, NULL),
-('Boucey', 'Paul', NULL, NULL, 2, NULL, NULL, NULL),
-('Boudaud', 'Jules', NULL, NULL, 1, NULL, NULL, NULL),
-('Bouland', 'Thomas', NULL, NULL, 2, NULL, NULL, NULL),
-('Bret', 'Vladimir', NULL, NULL, 2, NULL, NULL, NULL),
-('Briand', 'Dorian', NULL, NULL, 2, NULL, NULL, NULL),
-('Camera', 'Romain', NULL, NULL, 1, NULL, NULL, NULL),
-('Cassecuel', 'Clément', NULL, NULL, 2, NULL, NULL, NULL),
-('Choquet', 'Nicolas', NULL, NULL, 1, NULL, NULL, NULL),
-('Coquette', 'Yann', NULL, NULL, 1, NULL, NULL, NULL),
-('Duchatel', 'Mathis', NULL, NULL, 1, NULL, NULL, NULL),
-('Keller', 'Maxime', NULL, NULL, 1, NULL, NULL, NULL),
-('Lafont', 'Emeline', NULL, NULL, 2, NULL, NULL, NULL),
-('Laurain', 'Titouan', NULL, NULL, 2, NULL, NULL, NULL),
-('Le Coz', 'Samuel', NULL, NULL, 1, NULL, NULL, NULL),
-('Lecrivain', 'Ugo', NULL, NULL, 2, NULL, NULL, NULL),
-('Levielle', 'Cedric', NULL, NULL, 2, NULL, NULL, NULL),
-('Lisnic', 'Dorian', NULL, NULL, 2, NULL, NULL, NULL),
-('Maulnier', 'Leopold', NULL, NULL, 1, NULL, NULL, NULL),
-('Metayer', 'Coralie', 123, 12345, 1, 3, 0, 123),
-('Miché', 'Paul', NULL, NULL, 1, NULL, NULL, NULL),
-('Militao', 'Sarah', NULL, NULL, 1, NULL, NULL, NULL),
-('Morvan', 'Alan', NULL, NULL, 1, NULL, NULL, NULL),
-('Mouazan', 'Morgann', NULL, NULL, 1, NULL, NULL, NULL),
-('Ndao', 'Mouhamed', NULL, NULL, 1, NULL, NULL, NULL),
-('Parola', 'Mario', NULL, NULL, 1, NULL, NULL, NULL),
-('Passelande Porte', 'Baptiste', NULL, NULL, 2, NULL, NULL, NULL),
-('Polet', 'Allan', NULL, NULL, 1, NULL, NULL, NULL),
-('Raux', 'Kevin', NULL, NULL, 1, NULL, NULL, NULL),
-('Reintjes', 'Willian', NULL, NULL, 1, NULL, NULL, NULL),
-('Rogard', 'Olivier', NULL, NULL, 1, NULL, NULL, NULL),
-('Samson', 'Théo', NULL, NULL, 2, NULL, NULL, NULL),
-('Talio', 'Romain', NULL, NULL, 1, NULL, NULL, NULL),
-('Termet', 'Mathieu', NULL, NULL, 1, NULL, NULL, NULL),
-('Trinquart', 'Kilian', NULL, NULL, 1, NULL, NULL, NULL),
-('Vetele', 'Ewen', NULL, NULL, 2, NULL, NULL, NULL);
+INSERT INTO `etudiant` (`Nom`, `Prénom`, `photo`, `login`, `MDP`, `Groupe`, `absencesJ`, `absencesNJ`, `presence`) VALUES
+('Briand-Goudet', 'Loetis', 'loetisb', NULL, NULL, 1, NULL, NULL, 0),
+('Barreau', 'Pierre', 'pierreb', NULL, NULL, 1, NULL, NULL, 0),
+('Blanchon', 'Erwan', 'erwanb', NULL, NULL, 1, NULL, NULL, 0),
+('Boucey', 'Paul', 'paulb', NULL, NULL, 2, NULL, NULL, 0),
+('Boudaud', 'Jules', 'julesb', NULL, NULL, 1, NULL, NULL, 0),
+('Bouland', 'Thomas', 'thomasb', NULL, NULL, 2, NULL, NULL, 0),
+('Bret', 'Vladimir', 'vladimirb', NULL, NULL, 2, NULL, NULL, 0),
+('Briand', 'Dorian', 'dorianb', NULL, NULL, 2, NULL, NULL, 0),
+('Camera', 'Romain', 'romainc', NULL, NULL, 1, NULL, NULL, 0),
+('Cassecuel', 'Clément', 'clementc', NULL, NULL, 2, NULL, NULL, 0),
+('Choquet', 'Nicolas', 'nicolasc', NULL, NULL, 1, NULL, NULL, 0),
+('Coquette', 'Yann', 'yannc', NULL, NULL, 1, NULL, NULL, 0),
+('Duchatel', 'Mathis', 'mathisd', NULL, NULL, 1, NULL, NULL, 0),
+('Keller', 'Maxime', 'maximek', NULL, NULL, 1, NULL, NULL, 0),
+('Lafont', 'Emeline', 'emelinel', NULL, NULL, 2, NULL, NULL, 0),
+('Laurain', 'Titouan', 'titouanl', NULL, NULL, 2, NULL, NULL, 0),
+('Le Coz', 'Samuel', 'samuell', NULL, NULL, 1, NULL, NULL, 0),
+('Lecrivain', 'Ugo', 'ugol', NULL, NULL, 2, NULL, NULL, 0),
+('Levielle', 'Cedric', 'cedricl', NULL, NULL, 2, NULL, NULL, 0),
+('Lisnic', 'Dorian', 'dorianl', NULL, NULL, 2, NULL, NULL, 0),
+('Maulnier', 'Leopold', 'leopoldm', NULL, NULL, 1, NULL, NULL, 0),
+('Metayer', 'Coralie', 'coraliem', NULL, 12345, 1, 3, 0, 0),
+('Miché', 'Paul', 'paulm', NULL, NULL, 1, NULL, NULL, 0),
+('Militao', 'Sarah', 'sarahm', NULL, NULL, 1, NULL, NULL, 0),
+('Morvan', 'Alan', 'alanm', NULL, NULL, 1, NULL, NULL, 0),
+('Mouazan', 'Morgann', 'morgannm', NULL, NULL, 1, NULL, NULL, 0),
+('Ndao', 'Mouhamed', 'mouhamedn', NULL, NULL, 1, NULL, NULL, 0),
+('Parola', 'Mario', 'mariop', NULL, NULL, 1, NULL, NULL, 0),
+('Passelande Porte', 'Baptiste', 'baptistep', NULL, NULL, 2, NULL, NULL, 0),
+('Polet', 'Allan', 'allanp', NULL, NULL, 1, NULL, NULL, 0),
+('Raux', 'Kevin', 'kevinr', NULL, NULL, 1, NULL, NULL, 0),
+('Reintjes', 'Willian', 'williamr', NULL, NULL, 1, NULL, NULL, 0),
+('Rogard', 'Olivier', 'olivierr', NULL, NULL, 1, NULL, NULL, 0),
+('Samson', 'Théo', 'theos', NULL, NULL, 2, NULL, NULL, 0),
+('Talio', 'Romain', 'romaint', NULL, NULL, 1, NULL, NULL, 0),
+('Termet', 'Mathieu', 'mathieut', NULL, NULL, 1, NULL, NULL, 0),
+('Trinquart', 'Kilian', 'kiliant', NULL, NULL, 1, NULL, NULL, 0),
+('Vetele', 'Ewen', 'ewenv', NULL, NULL, 2, NULL, NULL, 0),
+('test', 'test', 'test', '123', 123, 1, NULL, NULL, 1),
+('Vanherzeele', 'Jacques', 'jacquesv', NULL, NULL, 2, NULL, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -109,16 +112,16 @@ DROP TABLE IF EXISTS `personnel`;
 CREATE TABLE IF NOT EXISTS `personnel` (
   `Nom` text CHARACTER SET utf8 NOT NULL,
   `Prénom` text CHARACTER SET utf8 NOT NULL,
-  `n°` int(11) DEFAULT NULL,
-  `MDP` text NOT NULL
+  `login` text CHARACTER SET utf8,
+  `MDP` text CHARACTER SET utf8 NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `personnel`
 --
 
-INSERT INTO `personnel` (`Nom`, `Prénom`, `n°`, `MDP`) VALUES
-('Prof', 'Prof', 1234, '1234');
+INSERT INTO `personnel` (`Nom`, `Prénom`, `login`, `MDP`) VALUES
+('Prof', 'Prof', '1234', '1234');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
