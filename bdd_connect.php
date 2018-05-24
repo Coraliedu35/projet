ee00c1b81579ba70cf62f1f756006a54079b4e38
mgskiller <mgskiller@gmail.com>
2018-05-24 13:09:36 +0200

    Update bdd_connect.php

--- bdd_connect.php
+++ bdd_connect.php
@@ -1,9 +1,11 @@
-<?php 
-
-    $bdd=mysqli_connect("localhost","root","","bdd_promo");
-    if (mysqli_connect_errno())
-      {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-      }
-
-?>
+    <?php
+    try
+    {
+    $bdd = new PDO('mysql:host=localhost;dbname=bdd_promo;charset=utf8', 'root', '',
+    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));                                    //lien avec la base de données
+    }
+    catch (Exception $e)                                                                    //en cas d'erreur on arrête la page
+    {
+            die('Erreur : ' . $e->getMessage());
+    }
+    ?>
