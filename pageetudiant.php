49674727838607a5b51b1141b3a6ac632a276252
Coraliedu35 <34239901+Coraliedu35@users.noreply.github.com>
2018-05-24 18:26:18 +0200

    Update pageetudiant.php

--- pageetudiant.php
+++ pageetudiant.php
@@ -2,23 +2,16 @@
     session_start();
 ?>
 
-*<!DOCTYPE html>
+<!DOCTYPE html>
 
 <html>
     <?php
-    try
-    {
-    $bdd = new PDO('mysql:host=localhost;dbname=bdd_promo;charset=utf8', 'root', '',
-    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));                                    //lien avec la base de données
-    }
-    catch (Exception $e)                                                                    //en cas d'erreur on arrête la page
-    {
-            die('Erreur : ' . $e->getMessage());
-    }
+        include('bdd_connect.php')
     ?>
     <head>
         <meta charset="utf-8" />
         <link rel="stylesheet" href="style.css" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <title>Statistiques d'absence</title>
     </head>
     <body>
@@ -34,38 +27,39 @@
             </thead>
             <tbody>
                 <?php
-                $reponse = $bdd->prepare('SELECT * FROM bdd_promo.etudiant WHERE login=?');
+                $reponse = $bdd->prepare('SELECT * FROM bdd_promo.etudiant,bdd_promo.absences WHERE etudiant.login=? AND etudiant.login=absences.loginetu');
                 $reponse->execute(array($_SESSION['login']));
-                while ($donnees = $reponse->fetch())    
+                while ($res =$reponse->fetch())    
                 {
                 ?>
                 <tr>
-                    <td><?php echo $donnees['absencesJ']; ?></td>
-                    <td><?php echo $donnees['absencesNJ']; ?></td>
-                    <td><?php echo $donnees['absencesNJ']+$donnees['absencesJ']; ?></td>
+                    <td><?php echo $res['j']; ?></td>
+                    <td><?php echo $res['nj']; ?></td>
+                    <td><?php echo $res['nj']+$res['j']; ?></td>
                 </tr>
                 <?php
                 }
-
-                $reponse->closeCursor(); // Termine le traitement de la requête
+                $reponse->closeCursor();                                                //Termine le traitement de la requête
                 ?>
             </tbody>
         </table>
-        <script>
-        function Deconnexion ()
-        {
-        function RedirigeDeconnexion()
-        {
-        document.location.href="deconnect.php"; 
-        }
-        if (confirm("Etes-vous sûr de vouloir vous déconnecter ?")) 
-            {
-                RedirigeDeconnexion();
-            }
-        }
-        </script>
-         <button onclick="Deconnexion()">Deconnexion</button> 
-    </body>
+        <a class="btn btn-outline-warning" href="modifs.php" role="button">Paramètres du compte</a>
 
+        <a class="btn btn-outline-info" href="faq.html" role="button">FAQ</a>
+        <script>
+                function Deconnexion ()
+                {
+                function RedirigeDeconnexion()
+                {
+                document.location.href="deconnect.php"; 
+                }
+                if (confirm("Etes-vous sûr de vouloir vous déconnecter ?")) 
+                    {
+                        RedirigeDeconnexion();
+                    }
+                }
+        </script>
+        <button class="btn btn-outline-primary" onclick="Deconnexion()">Deconnexion</button> 
+    </body>
 
 </html>
