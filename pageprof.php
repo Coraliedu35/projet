82559130ba2d3b136e5f87b215944824fc66769e
Coraliedu35 <34239901+Coraliedu35@users.noreply.github.com>
2018-05-24 13:45:58 +0200

    Update pageprof.php

--- pageprof.php
+++ pageprof.php
@@ -1,103 +1,68 @@
-<? php
-    session_start();
+<?php
+session_start (); // on démarre la session
+$login=$_POST['login'];
+$mdp=$_POST['mdp'];
+include('bdd_connect.php');
+    //Première étape : Authentification d'un enseignant
+    $verif_login = $bdd->prepare('SELECT COUNT(*) FROM bdd_promo.personnel WHERE login = ?'); //On vérifie que le login existe dans la table
+    $verif_login->execute(array($_POST['login']));
+    // Si le login rentré correspond à un login d'enseignant
+    if($verif_login->fetchColumn() != 0)  
+        {    
+        // Sélection du password pour le login saisi
+        $verif_mdp = $bdd->prepare('SELECT MDP FROM bdd_promo.personnel WHERE login = ? LIMIT 1'); // Préparation de la requête
+        $verif_mdp->execute(array($_POST['login'])); // Exécution
+        if ($_POST['mdp'] == $verif_mdp->fetchColumn()) // Si le mot de passe correspond (enseignant)
+            {
+                //on stocke les variables dans les variables de session
+                $_SESSION['login']=$_POST['login'];
+                $_SESSION['mdp']=$_POST['mdp'];
+                $verif_mdp->closeCursor(); // Termine le traitement de la requête mdp
+                header('Location: pageprof.php'); //on dirige vers la page principale
+            }
+        else
+            {
+                header('Location: accueil.php'); //On renvoie vers la page d'accueil
+                $verif_mdp->closeCursor(); // Termine le traitement de la requête login 
+                $_SESSION['essais']=$_SESSION['essais']+1;
+                
+            }
+        }
+     //Si le login ne correspond pas à un login d'enseignant
+     else 
+        {
+            //On vérifie que le login existe dans la table étudiant
+            $verif_login = $bdd->prepare('SELECT COUNT(*) FROM bdd_promo.etudiant WHERE login = ?'); 
+            $verif_login->execute(array($_POST['login']));
+            // Si le login existe dans la table étudiant
+            if($verif_login->fetchColumn() !=0) 
+            {    
+                // Sélection du password pour le login saisi
+                $verif_mdp = $bdd->prepare('SELECT MDP FROM bdd_promo.etudiant WHERE login = ? LIMIT 1'); // Préparation de la requête
+                $verif_mdp->execute(array($_POST['login'])); // Exécution
+                 //Si le mot correpond au mot de passe d'un étudiant
+                if ($_POST['mdp'] == $verif_mdp->fetchColumn())
+                    {
+                     //on stocke les variables dans les variables de session
+                    $_SESSION['login']=$_POST['login'];
+                    $_SESSION['mdp']=$_POST['mdp'];
+                    $verif_mdp->closeCursor(); // Termine le traitement de la requête mdp
+                    header('Location: pageetudiant.php'); //on dirige vers la page principale
+                    }
+                else
+                {
+                    header('Location: accueil.php'); // Si il manque login ou mdp, on renvoie vers la page d'accueil
+                    $verif_mdp->closeCursor(); // Termine le traitement de la requête mdp
+                    $_SESSION['essais']=$_SESSION['essais']+1;
+                }  
+            }
+            // Si le login n'existe pas dans la table étudiant
+            else
+            {
+                $_SESSION['essais']=$_SESSION['essais']+1;
+                $verif_login->closeCursor(); // Termine le traitement de la requête mdp
+                header('Location: accueil.php'); // Si il manque login ou mdp, on renvoie vers la page d'accueil
+                
+            }
+        }
 ?>
-
-
-<!DOCTYPE html>
-
-<html>
-     <head>
-        <meta charset="utf-8" />
-        <link rel="stylesheet" href="style.css" />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <title>Absence</title>
-    </head>
-    <?php
-        include('bdd_connect.php');
-    ?>
-    <body>
-        <header> IUT de Saint-Malo </header>
-        <!--<img src="logo2.jpg" title="logo2" /> -->
-        <h1>Feuille d'absence</h1>
-        <h4><a href="trombi-etudiant.php">Trombi des étudiants manquants</a></h4>
-        <form method="POST" action="verif.php">
-            <a href="qrcode.php">Lien QR </a>
-        <table align="center">
-            <thead>
-                <tr>
-                    <td colspan="4"="4">Promotion</td>
-                </tr>
-                <tr>
-                    <td colspan="4"="4>FI1A</td>
-                </tr>
-                <tr>
-                    <td>Nom</td>
-                    <td>Prénom</td>
-                    <td>Photo</td>
-                    <td>Présence</td>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td colspan="4" ="4">Groupe 1</td>
-                </tr>
-                <?php
-                $reponse = mysqli_query($bdd,'SELECT * FROM etudiant WHERE Groupe=1 ORDER BY Nom');                                  //On récupère le contenu du tableau etudiant de la bdd
-                while ($res = mysqli_fetch_assoc($reponse))
-                {
-
-                ?>
-                <tr>
-                    <td><?php echo $res['Nom']; ?>
-                        <li>
-                            <ul>nombre d'absences justifiées : <?php echo $res['absencesJ']; ?></ul>
-                            <ul>nombre d'absences injustifiées : <?php echo $res['absencesNJ']; ?></ul>
-                        </li>
-                    </td>
-                    <td><?php echo $res['prenom']; ?></td>
-                    <td><img src="trombi/<?php echo $res['photo']; ?>.png" title="trombi"/></td>
-                    <td>
-                        <input type="checkbox" name="absent" id="absent" />
-                    </td>
-                </tr>
-                <?php
-                }
-                ?>
-                <tr>
-                    <td colspan="4" ="4">Groupe 2</td>
-                </tr>
-                <?php
-                $reponse = mysqli_query($bdd,'SELECT * FROM etudiant WHERE Groupe=2 ORDER BY Nom');                                  //On récupère le contenu du tableau etudiant de la bdd
-                while ($res = mysqli_fetch_assoc($reponse))
-                {
-                ?>
-                <tr>
-                    <td><?php echo $res['Nom']; ?>
-                       <li>
-                            <ul>nombre d'absences justifiées : <?php echo $res['absencesJ']; ?></ul>
-                            <ul>nombre d'absences injustifiées : <?php echo $res['absencesNJ']; ?></ul>
-                        </li>
-                    </td>
-                    <td><?php echo $res['prenom']; ?></td>
-                    <td><img src="trombi/<?php echo $res['photo']; ?>.png" title="trombi"/></td>
-                    <td>
-                        <input type="checkbox" name="absent" id="absent" /> 
-                    </td>
-                </tr>
-                <?php
-                }
-
-                mysqli_close($bdd); // Termine le traitement de la requête
-                ?>
-
-            </tbody>
-        </table>
-        <input type="submit" class="btn btn-success" value="envoyer" /></form>
-        <!--bouton de déconnexion : voir à créer un pop-up "êtes-vous sûr"-->
-        <a class="btn btn-outline-primary" href="deconnect.php" role="button">Déconnexion</a>
-
-        <a class="btn btn-outline-warning" href="faq.html" role="button">FAQ</a>
-    </body>
-
-
-</html>
