e190bad4d6b105371755ccf61f8d1761437da898
Coraliedu35 <34239901+Coraliedu35@users.noreply.github.com>
2018-05-24 16:05:56 +0200

    Update qrcode.php

--- qrcode.php
+++ qrcode.php
@@ -1,4 +1,8 @@
 <?php
 	include ("repqr/qrlib.php");
-	QRcode::png("connect.php");
+
+	$char = 'abcdefghijklmnopqrstuvwxyz0123456789'; //permet d'avoir une variable GET aléatoire afin de ne pas pouvoir connaître le lien à l'avance
+	$v = str_shuffle($char);
+
+	QRcode::png("index.php?v=$v");
 ?>
