f0d327c8c94d81fb7bc8f0d47f79734a6b6d9306
mgskiller <mgskiller@gmail.com>
2018-05-28 08:28:21 +0200

    Add files via upload

--- qrcode.php
+++ qrcode.php
@@ -1,8 +1,6 @@
 <?php
 	include ("repqr/qrlib.php");
-
 	$char = 'abcdefghijklmnopqrstuvwxyz0123456789'; //permet d'avoir une variable GET aléatoire afin de ne pas pouvoir connaître le lien à l'avance
 	$v = str_shuffle($char);
-
-	QRcode::png("index.php?v=$v");
+	QRcode::png("accueil.php?v=$v");
 ?>
