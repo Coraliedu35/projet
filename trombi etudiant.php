40d9f0c61c7237457c3c2069b14ecebd973e6ac1
NicoChoq <34240252+NicoChoq@users.noreply.github.com>
2018-05-29 09:51:07 +0200

    Update trombi etudiant.php

--- trombi etudiant.php
+++ trombi etudiant.php
@@ -1,9 +1,10 @@
-<!Doctype>
+<!DOCTYPE html>
 <html>
     <header>
         <link rel="stylesheet" href="style.css" />
     </header>
     <?php
+        session_start();
         try
         {
         $bdd = new PDO('mysql:host=localhost;dbname=bdd_promo;charset=utf8', 'root', '',
@@ -13,11 +14,10 @@
         {
             die('Erreur : ' . $e->getMessage());
         }
-
         $reponse = $bdd->query('SELECT * FROM bdd_promo.etudiant order by Nom');                                  //On récupère le contenu du tableau etudiant de la bdd
         while ($donnees = $reponse->fetch())
         {
-            if ($donnees['presence'] == 1) 
+            if ($donnees['presencetemp'] == 0) 
             {
         ?>
 
@@ -25,8 +25,23 @@
         <?php
                 echo $donnees['Nom']," ";
                 echo $donnees['Prénom'];
-
             }
+        ?>
+        <form method="post" action="trombi.php">
+           <p>
+               <select name="etat" >
+                   <option value="2">Abscence justifié</option>
+                   <option value="3">Abscence injustifié</option>
+                   <option value="4">Retard</option>
+               </select>
+           </p>
+        </form>
+        <?php
+            $login= $donnees['Nom'];
+            $presencetemp= $_POST['value']
+            $req = $bdd->prepare('UPDATE bdd_promo.etudiant SET `presencetemp` = ? WHERE login = ?');
+            $req->execute(array($presencetemp, $login));
+        ?>
         }
     ?>
 </html>
