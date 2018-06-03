774b7e8638b1c7f2eba5e9624f2d871fa6956512
mgskiller <mgskiller@gmail.com>
2018-06-03 11:14:20 +0200

    Add files via upload

--- ecriture.php
+++ ecriture.php
@@ -4,6 +4,17 @@
 	$mdp=$_POST['mdp'];
 	$v=$_SESSION['v'];
 	include('bdd_connect.php');
+    // Récupéraiton du temps de génération du QR code
+    $req_temps_ref = $bdd-> prepare('SELECT horaire FROM qrcode WHERE id_cours = ? LIMIT 1');
+    $req_temps_ref -> execute(array($POST['id_cours']));
+    $array_temps_ref = $req_temps_ref -> fetch();
+    //print_r($array_temps_ref);
+    $chaine_temps_ref=implode(" ", $array_temps_ref);
+    $chaine_temps_ref=substr($chaine_temps_ref, 19);
+    $temps_ref=strtotime($chaine_temps_ref);
+    echo $temps_ref;
+    $temps=time();
+    echo $temps;
    	//On vérifie que le login existe dans la table étudiant
     $verif_login = $bdd->prepare('SELECT COUNT(*) FROM bdd_promo.etudiant WHERE login = ?'); 
     $verif_login->execute(array($_POST['login']));
@@ -16,9 +27,9 @@
         //Si le mot correpond au mot de passe d'un étudiant
             if ($_POST['mdp'] == $verif_mdp->fetchColumn())
  	           {
-               //on stocke les variables dans les variables de session
-                $_SESSION['login']=$_POST['login'];
-                $_SESSION['mdp']=$_POST['mdp'];
+                // si la date correspond (moins de 30 secondes après génération du QR code)
+                if ($temps<=$temps_ref+30)
+                {
                 $verif_mdp->closeCursor(); // Termine le traitement de la requête mdp              
 				$req = $bdd->prepare('UPDATE bdd_promo.etudiant SET `presencetemp` = 1 WHERE login = ?');
 				$req->execute(array($login));
