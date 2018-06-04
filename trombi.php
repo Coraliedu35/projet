462de2f4ac4df3262113f5a10a6f550fe2fefa3e
Coraliedu35 <34239901+Coraliedu35@users.noreply.github.com>
2018-06-04 18:45:30 +0200

    Update trombi.php

--- trombi.php
+++ trombi.php
@@ -5,25 +5,18 @@
     </header>
     <?php
         session_start();
-        try
-        {
-        $bdd = new PDO('mysql:host=localhost;dbname=bdd_promo;charset=utf8', 'root', '',
-        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));                                    //lien avec la base de données
-        }
-        catch (Exception $e)                                                                    //en cas d'erreur on arrête la page
-        {
-            die('Erreur : ' . $e->getMessage());
-        }
+        include('bdd_connect.php');
+
         $reponse = $bdd->query('SELECT * FROM bdd_promo.etudiant order by Nom');                                  //On récupère le contenu du tableau etudiant de la bdd
         while ($donnees = $reponse->fetch())
         {
             if ($donnees['presencetemp'] == 0) 
             {
-        ?>
+    ?>
 
-        <img src="<?php echo $donnees['photo'] ?>.jpg" alt="photo_etudiant"/>
+        <img src="<?php echo $donnees['photo']; ?>.jpg" alt="photo_etudiant"/>
         <?php
-                echo $donnees['Nom']," ";
+                echo $donnees['Nom']." ";
                 echo $donnees['Prénom'];
             }
         ?>
@@ -38,10 +31,9 @@
         </form>
         <?php
             $login= $donnees['Nom'];
-            $presencetemp= $_POST['value']
+            $presencetemp= $_POST['etat'];
             $req = $bdd->prepare('UPDATE bdd_promo.etudiant SET `presencetemp` = ? WHERE login = ?');
             $req->execute(array($presencetemp, $login));
-        ?>
         }
-    ?>
+        ?>
 </html>
