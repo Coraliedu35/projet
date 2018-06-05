c6da571167ad5f7b7f9b7934ff0e11ff3a46cb59
Coraliedu35 <34239901+Coraliedu35@users.noreply.github.com>
2018-06-05 14:52:44 +0200

    Update pageprof.php

--- pageprof.php
+++ pageprof.php
@@ -14,56 +14,65 @@
     <body>
         <header> IUT de Saint-Malo </header>
         <h1>Feuille d'absence</h1>
-        <h4><a href="trombi-etudiant.php">Trombi des étudiants manquants</a></h4>
+        <h4><a href="trombi.php">Trombi des étudiants manquants</a></h4>
         <form method="POST" action="verif.php">
             <a href="qrcode.php">Lien QR </a>
 
                 <?php
                 /*traitement de la chaîne de caractère, identification promo*/
-                $promo = $_GET['promo'];
-                $gr=substr($promo,-1);
-                $promo=substr($promo,0,-1);
+                $cours = $_SESSION['id_cours'];
+                $rep=$bdd->prepare('SELECT `id_promo` FROM `cours` WHERE `id_cours` = ?');
+                $rep->execute(array($cours));
+                while($res=$rep->fetch()){
+                    $promo=$res['id_promo'];
 
-                $reponse = $bdd->prepare('SELECT * FROM bdd_promo.etudiant WHERE Groupe= ? AND id_promo = ? ORDER BY Nom');         //On récupère le contenu du tableau etudiant de la bdd
+                    /*cours en groupe*/
+                    if(strlen($promo)==5){
+                        $gr=substr($promo,-1);
+                        $promo=substr($promo,0,-2).'A';        
+                    }
+                    /*cours en classe entière*/
+                    else if(substr($promo,0,3)=='R&T'){
+                        $promo=substr($promo,-4);
+                    }
+                }
+                /*affichage des étudiants en fonction de leur promo et leur groupe*/
+                $reponse = $bdd->prepare('SELECT * FROM bdd_promo.etudiant WHERE Groupe= ? AND id_promo = ? ORDER BY Nom');
                 $reponse->execute(array($gr,$promo));
                 ?>
             <table align="center">
                 <thead>
                     <tr>
-                        <td colspan="4"="4">Promotion</td>
+                        <td colspan="3"="3">Promotion</td>
                     </tr>
                     <tr>
-                        <td colspan="4"="4><?php echo $promo; ?></td>
+                        <td colspan="3"><?php echo $promo; ?></td>
                     </tr>
                     <tr>
                         <td>Nom</td>
                         <td>Prénom</td>
                         <td>Photo</td>
-                        <td>Présence</td>
                     </tr>
                 </thead>
                 <tbody>
                 <tr>
-                    <td colspan="4" ="4">Groupe <?php echo $gr;?></td>
+                    <td colspan="3" ="3">Groupe <?php echo $gr;?></td>
                 </tr>
                 <?php
                 while ($res = $reponse->fetch())                                                                //On affiche une nouvelle ligne par étudiant trouvé
                 {
-                    $abs = $bdd->prepare('SELECT * FROM `absences`, `etudiant` WHERE etudiant.photo = ? AND etudiant.login=absences.loginetu');
+                    $abs = $bdd->prepare('SELECT * FROM `absences`, `etudiant` WHERE etudiant.photo = ? AND etudiant.Nom=absences.loginetu');
                     $abs->execute(array($res['photo']));
                 ?>
                 <tr>
                     <td><?php echo $res['Nom']; ?>
                         <li>
-                            <ul>nombre d'absences justifiées : <?php echo $res['j']; ?></ul>
-                            <ul>nombre d'absences injustifiées : <?php echo $res['nj']; ?></ul>
+                            <ul>nombre d'absences justifiées : <?php echo 'j'; ?></ul>
+                            <ul>nombre d'absences injustifiées : <?php echo 'nj'; ?></ul>
                         </li>
                     </td>
                     <td><?php echo $res['Prénom']; ?></td>
                     <td><img src="trombi/<?php echo $res['photo']; ?>.png" title="trombi"/></td>
-                    <td>
-                        <input type="checkbox" name="absent" id="<?php echo $res['login']; ?>" />
-                    </td>
                 </tr>
                 <?php
                 }
@@ -74,9 +83,24 @@
         </table>
         <input type="submit" class="btn btn-success" value="envoyer" /></form>
         <!--bouton de déconnexion : voir à créer un pop-up "êtes-vous sûr"-->
-        <a class="btn btn-outline-primary" href="deconnect.php" role="button">Déconnexion</a>
+        <script>
+                function Deconnexion ()
+                {
+                function RedirigeDeconnexion()
+                {
+                document.location.href="deconnect.php"; 
+                }
+                if (confirm("Etes-vous sûr de vouloir vous déconnecter ?")) 
+                    {
+                        RedirigeDeconnexion();
+                    }
+                }
+        </script>
+        <button class="btn btn-outline-primary" href="deconnect.php" onclick="Deconnexion()">Déconnexion</button>
 
         <a class="btn btn-outline-warning" href="faq.html" role="button">FAQ</a>
+
+        <a class="btn btn-outline-warning" href="modifs.php" role="button">Paramètres du compte</a>
     </body>
 
 
