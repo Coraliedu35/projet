b0d252e87b79287c3b35beb22349ebc143b1fc2c
Coraliedu35 <34239901+Coraliedu35@users.noreply.github.com>
2018-06-06 12:10:54 +0200

    Update pageprof.php

--- pageprof.php
+++ pageprof.php
@@ -9,18 +9,20 @@
         <meta charset="utf-8" />
         <link rel="stylesheet" href="style.css" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <title>Absence</title>
+        <title>Enseignant</title>
     </head>
     <body>
+        <?php
+            $cours = $_SESSION['id_cours'];
+        ?>
         <header> IUT de Saint-Malo </header>
         <h1>Feuille d'absence</h1>
         <h4><a href="trombi.php">Trombi des étudiants manquants</a></h4>
         <form method="POST" action="verif.php">
-            <a href="qrcode.php">Lien QR </a>
+            <a href="qrcode.php?c=<?php echo $cours;?>">Lien QR </a>
 
                 <?php
                 /*traitement de la chaîne de caractère, identification promo*/
-                $cours = $_SESSION['id_cours'];
                 $rep=$bdd->prepare('SELECT `id_promo` FROM `cours` WHERE `id_cours` = ?');
                 $rep->execute(array($cours));
                 while($res=$rep->fetch()){
@@ -29,16 +31,18 @@
                     /*cours en groupe*/
                     if(strlen($promo)==5){
                         $gr=substr($promo,-1);
-                        $promo=substr($promo,0,-2).'A';        
+                        $promo=substr($promo,0,-2).'A';   
+                        /*affichage des étudiants en fonction de leur promo et leur groupe*/
+                        $reponse = $bdd->prepare('SELECT * FROM bdd_promo.etudiant WHERE Groupe= ? AND id_promo = ? ORDER BY Nom');
+                        $reponse->execute(array($gr,$promo));  
                     }
                     /*cours en classe entière*/
                     else if(substr($promo,0,3)=='R&T'){
                         $promo=substr($promo,-4);
+                        $reponse = $bdd->prepare('SELECT * FROM bdd_promo.etudiant WHERE id_promo = ? ORDER BY Nom');
+                        $reponse->execute(array($promo));
                     }
                 }
-                /*affichage des étudiants en fonction de leur promo et leur groupe*/
-                $reponse = $bdd->prepare('SELECT * FROM bdd_promo.etudiant WHERE Groupe= ? AND id_promo = ? ORDER BY Nom');
-                $reponse->execute(array($gr,$promo));
                 ?>
             <table align="center">
                 <thead>
@@ -56,19 +60,32 @@
                 </thead>
                 <tbody>
                 <tr>
-                    <td colspan="3" ="3">Groupe <?php echo $gr;?></td>
+                    <td colspan="3" ="3"><?php if(isset($gr)){ echo 'Groupe'.$gr;} else{ echo " Classe entière";}?></td>
                 </tr>
                 <?php
-                while ($res = $reponse->fetch())                                                                //On affiche une nouvelle ligne par étudiant trouvé
+                while ($res = $reponse->fetch())                                         //On affiche une nouvelle ligne par étudiant trouvé
                 {
-                    $abs = $bdd->prepare('SELECT * FROM `absences`, `etudiant` WHERE etudiant.photo = ? AND etudiant.Nom=absences.loginetu');
-                    $abs->execute(array($res['photo']));
+
+                $abs = $bdd->prepare('SELECT * FROM bdd_promo.etudiant,bdd_promo.absences WHERE etudiant.login=? AND etudiant.login=absences.loginetu');
+                $abs->execute(array($res['login']));
+                $jtot=0;
+                $njtot=0;
+                $atot=0;
+
                 ?>
                 <tr>
-                    <td><?php echo $res['Nom']; ?>
+                    <td><?php echo $res['Nom']; 
+                        while($rep=$abs->fetch())
+                        {
+                            $njtot=$njtot+$rep['nj'];
+                            $jtot=$jtot+$rep['j'];
+                            $atot=$atot+1;
+                        }
+                    ?>
                         <li>
-                            <ul>nombre d'absences justifiées : <?php echo 'j'; ?></ul>
-                            <ul>nombre d'absences injustifiées : <?php echo 'nj'; ?></ul>
+                            <ul>nombre d'absences justifiées : <?php echo $jtot; ?></ul>
+                            <ul>nombre d'absences injustifiées : <?php echo $njtot; ?></ul>
+                            <ul>absences totales : <?php echo $atot; ?></ul>
                         </li>
                     </td>
                     <td><?php echo $res['Prénom']; ?></td>
