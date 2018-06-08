e2e4a64e67c5836f1ee5e7da0139f09fb2dd525b
Coraliedu35 <34239901+Coraliedu35@users.noreply.github.com>
2018-06-08 15:10:56 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -1,14 +1,6 @@
 <?php
-    session_start(); // on démarre la session
-    if(empty($_SESSION['essais'])) 
-    {
-        $_SESSION['essais'] = 0;
-    }
-    else if($_SESSION['essais'] > 3) 
-    {        
-        exit('trop de tentatives');
-    }   
-?>
+ session_start (); // on démarre la session
+ ?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -18,16 +10,15 @@
         <title>Gestion des absences : authentification</title>
     </head>
     <body>
-        <header>IUT de Saint-Malo</header>
+        <header> Département R&T FI1A </header>
         <h1>Gestion des absences</h1>
          <!--formulaire qui envoie les information vers le script d'authentification : login et mdp-->
         <form method="post" action="connect.php">
             <fieldset>
             <input type="text" name="login" placeholder="N° SESAME"/> <br/> 
             <input type="password" name="mdp" placeholder="Mot de passe" /> <br/>
-            <label for="remember">Se souvenir de moi</label> <input type="checkbox" name="remember" id="remember" />  <br/>
             <br/>
-            <input type="submit" value="Valider" />    
+            <input type="submit" value="Valider" class="btn-warning btn-input" />    
             </fieldset>   
         </form> 
     </body>
