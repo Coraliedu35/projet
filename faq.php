f936d1d48b478725179e70a45f82e2d8220e57ea
NicoChoq <34240252+NicoChoq@users.noreply.github.com>
2018-06-08 13:17:54 +0200

    Add files via upload

---
+++ faq.php
@@ -0,0 +1,28 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <link rel="shortcut icon" type="image/x-icon" href="logo3.png" />
+        <title>FAQ</title>
+    </head>
+    <body class="contenu">
+        <header>IUT de Saint-Malo</header>
+            <h2>1) Comment changer mon mot de passe?</h2>
+            <p id="azerty">Pour vous connecter à votre compte veuillez <a href="modif.php">cliquer ici</a>  ou aller sur la page parametre du compte puis rentrer votre ancien mot-de-passe puis votre nouveau mot-de-passe.</p>
+        	<h2>2) Comment valider ma presence?</h2>
+            <p id="azerty">Au debut du cours, le professeur va projeter un QRcode que vous devrez scanner. Ce QRcode va vous envoyer sur une page ou vous devrez rentrer votre login et votre mot-de-passe.</p>
+            <h2>3) Comment changer l'état d'un étudiant?</h2>
+            <p id="azerty">Afin de changer l'état d'un étudiant, il suffirat d'aller sur la page trombi puis de sélectionner l'état de l'étudiant dans le menu déroulant.</p>
+            <h2>4) ?</h2>
+            <p id="azerty">Pour vous connecter à votre compte veuillez <a href="connect.php">cliquer ici</a>  ou aller sur la page se connecter puis rentrer votre login et votre mot-de-passe.</p>
+    
+
+
+
+        <a class="btn-warning btn-outline" href="javascript:history.go(-1)" role="button">retour</a>
+    </body>
+</html>
